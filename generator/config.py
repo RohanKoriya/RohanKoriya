2d19bf266ef9d70c0d657cc46962ea4a6aeff917
RohanKoriya <koriyarohan123@gmail.com>
2026-07-28 18:36:20 +0530

    test

--- generator/config.py
+++ generator/config.py
@@ -75,7 +75,7 @@ THEME = {
 CANVAS_WIDTH = 1000
 CANVAS_HEIGHT = 820
 SIDEBAR_WIDTH = 300
-SHOW_SIDEBAR = False
+SHOW_SIDEBAR = True
 
 # ── Output paths ─────────────────────────────────────────────────────
 OUTPUT_DIR = "output"
